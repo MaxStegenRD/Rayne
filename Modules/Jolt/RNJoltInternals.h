45a1dcdc7228597781f89bc693cb5df5514b40dd
Nils Daumann <nils_daumann@slindev.com>
2025-03-08 14:58:32 +0100

    Fixed compiling of the Jolt module and updated to the latest version.

--- Modules/Jolt/RNJoltInternals.h
+++ Modules/Jolt/RNJoltInternals.h
@@ -12,10 +12,10 @@
 #include "RNJolt.h"
 #include "RNJoltKinematicController.h"
 
+#include <Jolt/Jolt.h>
 #include <Jolt/Core/Factory.h>
 #include <Jolt/Core/JobSystemThreadPool.h>
 #include <Jolt/Core/TempAllocator.h>
-#include <Jolt/Jolt.h>
 #include <Jolt/Physics/Body/BodyActivationListener.h>
 #include <Jolt/Physics/Body/BodyCreationSettings.h>
 #include <Jolt/Physics/Character/CharacterVirtual.h>
