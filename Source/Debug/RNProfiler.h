061af033798fe0c703777ac65979c6b8a80c817b
Nils Daumann <nils_daumann@slindev.com>
2025-11-23 10:57:25 +0100

    Disabled tracy for metal parts for now as they would require arc which requires more renderer changes.

--- Source/Debug/RNProfiler.h
+++ Source/Debug/RNProfiler.h
@@ -30,16 +30,24 @@
 	// Note: Ensure the respective Tracy GPU header is included in the translation
 	// unit using these (TracyMetal.hpp / TracyVulkan.hpp).
 	// ---------------------------
-	#if RN_BUILD_METAL
-		#include <tracy/TracyMetal.hpp>
+	/*#if RN_BUILD_METAL
+		//Requires ARC, but then other things break.
+		#include <tracy/TracyMetal.hmm>
 		#define RN_PROFILE_METAL_CONTEXT_TYPE TracyMetalCtx
 
 		#define RN_PROFILE_METAL_DECLARE_CONTEXT(ctxVar, device) TracyMetalContext(device)
-		#define RN_PROFILE_METAL_COLLECT(ctxVar)                                 TracyMetalCollect(ctxVar)
-		#define RN_PROFILE_METAL_SCOPE(ctxVar, cmdBuf)                    TracyMetalZone(ctxVar, cmdBuf, __FUNCTION__)
-		#define RN_PROFILE_METAL_SCOPE_N(ctxVar, cmdBuf, name)            TracyMetalZone(ctxVar, cmdBuf, name)
-		#define RN_PROFILE_METAL_DESTROY_CONTEXT(ctxVar)                  TracyMetalDestroy(ctxVar)
-	#endif
+		#define RN_PROFILE_METAL_COLLECT(ctxVar) TracyMetalCollect(ctxVar)
+		#define RN_PROFILE_METAL_SCOPE(ctxVar, cmdBuf) TracyMetalZone(ctxVar, cmdBuf, __FUNCTION__)
+		#define RN_PROFILE_METAL_SCOPE_N(ctxVar, cmdBuf, name) TracyMetalZone(ctxVar, cmdBuf, name)
+		#define RN_PROFILE_METAL_DESTROY_CONTEXT(ctxVar) TracyMetalDestroy(ctxVar)
+	#else*/
+		#define RN_PROFILE_METAL_CONTEXT_TYPE (void*)
+		#define RN_PROFILE_METAL_DECLARE_CONTEXT(ctxVar, device) nullptr
+		#define RN_PROFILE_METAL_COLLECT(ctxVar) ((void)0)
+		#define RN_PROFILE_METAL_SCOPE(ctxVar, cmdBuf) ((void)0)
+		#define RN_PROFILE_METAL_SCOPE_N(ctxVar, cmdBuf, name) ((void)0)
+		#define RN_PROFILE_METAL_DESTROY_CONTEXT(ctxVar) ((void)0)
+	//#endif
 
 	#if RN_BUILD_VULKAN
 		#include <vulkan.h>
@@ -48,10 +56,17 @@
 		#define RN_PROFILE_VULKAN_CONTEXT_TYPE TracyVkCtx
 
 		#define RN_PROFILE_VULKAN_DECLARE_CONTEXT(instance, device, physicalDevice, queue, cmdBuf, instanceProcAddr, deviceProcAddr) TracyVkContext(instance, physicalDevice, device, queue, cmdBuf, instanceProcAddr, deviceProcAddr)
-		#define RN_PROFILE_VULKAN_COLLECT(ctxVar, cmdBuf)                                TracyVkCollect(ctxVar, cmdBuf)
-		#define RN_PROFILE_VULKAN_SCOPE_CMD(ctxVar, cmdBuf)                              TracyVkZone(ctxVar, cmdBuf, __FUNCTION__)
-		#define RN_PROFILE_VULKAN_SCOPE_CMD_N(ctxVar, cmdBuf, name)                      TracyVkZone(ctxVar, cmdBuf, name)
-		#define RN_PROFILE_VULKAN_DESTROY_CONTEXT(ctxVar)                                TracyVkDestroy(ctxVar)
+		#define RN_PROFILE_VULKAN_COLLECT(ctxVar, cmdBuf) TracyVkCollect(ctxVar, cmdBuf)
+		#define RN_PROFILE_VULKAN_SCOPE_CMD(ctxVar, cmdBuf) TracyVkZone(ctxVar, cmdBuf, __FUNCTION__)
+		#define RN_PROFILE_VULKAN_SCOPE_CMD_N(ctxVar, cmdBuf, name) TracyVkZone(ctxVar, cmdBuf, name)
+		#define RN_PROFILE_VULKAN_DESTROY_CONTEXT(ctxVar) TracyVkDestroy(ctxVar)
+	#else
+		#define RN_PROFILE_VULKAN_CONTEXT_TYPE (void*)
+		#define RN_PROFILE_VULKAN_DECLARE_CONTEXT(instance, device, physicalDevice, queue, cmdBuf, instanceProcAddr, deviceProcAddr) nullptr
+		#define RN_PROFILE_VULKAN_COLLECT(ctxVar, cmdBuf) ((void)0)
+		#define RN_PROFILE_VULKAN_SCOPE_CMD(ctxVar, cmdBuf) ((void)0)
+		#define RN_PROFILE_VULKAN_SCOPE_CMD_N(ctxVar, cmdBuf, name) ((void)0)
+		#define RN_PROFILE_VULKAN_DESTROY_CONTEXT(ctxVar) ((void)0)
 	#endif
 
 // ---------------------------
@@ -65,14 +80,14 @@
 	#define RN_PROFILE_FRAME_TRACY() ((void)0)
 
 	#define RN_PROFILE_METAL_CONTEXT_TYPE (void*)
-	#define RN_PROFILE_METAL_DECLARE_CONTEXT(ctxVar, device, cmdBuf) ((void)0)
+	#define RN_PROFILE_METAL_DECLARE_CONTEXT(ctxVar, device, cmdBuf) nullptr
 	#define RN_PROFILE_METAL_COLLECT(ctxVar) ((void)0)
 	#define RN_PROFILE_METAL_SCOPE(ctxVar, cmdBuf) ((void)0)
 	#define RN_PROFILE_METAL_SCOPE_N(ctxVar, cmdBuf, name) ((void)0)
 	#define RN_PROFILE_METAL_DESTROY_CONTEXT(ctxVar) ((void)0)
 
 	#define RN_PROFILE_VULKAN_CONTEXT_TYPE (void*)
-	#define RN_PROFILE_VULKAN_DECLARE_CONTEXT(instance, device, physicalDevice, queue, cmdBuf, instanceProcAddr, deviceProcAddr) ((void)0)
+	#define RN_PROFILE_VULKAN_DECLARE_CONTEXT(instance, device, physicalDevice, queue, cmdBuf, instanceProcAddr, deviceProcAddr) nullptr
 	#define RN_PROFILE_VULKAN_COLLECT(ctxVar, cmdBuf) ((void)0)
 	#define RN_PROFILE_VULKAN_SCOPE_CMD(ctxVar, cmdBuf) ((void)0)
 	#define RN_PROFILE_VULKAN_SCOPE_CMD_N(ctxVar, cmdBuf, name) ((void)0)
