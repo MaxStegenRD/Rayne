5970f19a0ff0eb39e5efc197fa39f8fa9f0f8c65
Nils Daumann <nils_daumann@slindev.com>
2026-05-21 12:55:46 +0200

    Another attempt to make resource copying just work.

--- CMake/Rayne.cmake
+++ CMake/Rayne.cmake
@@ -15,22 +15,6 @@ function(rayne_get_android_assets_directory _OUT_VARIABLE)
     set("${_OUT_VARIABLE}" "${_ANDROID_ASSETS_DIRECTORY}" PARENT_SCOPE)
 endfunction()
 
-function(INTERNAL_rayne_stage_android_asset _SOURCE _DESTINATION)
-    get_filename_component(_DESTINATION_DIRECTORY "${_DESTINATION}" DIRECTORY)
-    file(MAKE_DIRECTORY "${_DESTINATION_DIRECTORY}")
-
-    if(IS_DIRECTORY "${_SOURCE}")
-        file(GLOB_RECURSE _RAYNE_ANDROID_ASSET_INPUTS LIST_DIRECTORIES false CONFIGURE_DEPENDS "${_SOURCE}/*")
-        file(REMOVE_RECURSE "${_DESTINATION}")
-        file(COPY "${_SOURCE}" DESTINATION "${_DESTINATION_DIRECTORY}")
-    elseif(EXISTS "${_SOURCE}")
-        set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${_SOURCE}")
-        file(COPY_FILE "${_SOURCE}" "${_DESTINATION}" ONLY_IF_DIFFERENT)
-    else()
-        message(FATAL_ERROR "Android asset source does not exist: ${_SOURCE}")
-    endif()
-endfunction()
-
 macro(rayne_link_with _TARGET)
     if(IOS OR VISIONOS)
         get_target_property(CURRENT_EMBED_FRAMEWORKS ${_TARGET} XCODE_EMBED_FRAMEWORKS)
@@ -163,15 +147,54 @@ macro(rayne_use_modules _TARGET _MODULES)
 
         if(ANDROID)
             rayne_get_android_assets_directory(android-assets-dir)
-            get_target_property(_MODULE_RESOURCE_SOURCE_DIRECTORY ${_MODULE_TARGET} SOURCE_DIR)
-            foreach(_RESOURCE ${${_MODULE}_MODULE_RESOURCES})
-                INTERNAL_rayne_stage_android_asset("${_MODULE_RESOURCE_SOURCE_DIRECTORY}/${_RESOURCE}" "${android-assets-dir}/Resources/Modules/${_MODULE}/${_RESOURCE}")
-            endforeach()
+            set(_MODULE_RESOURCE_COPY_TARGET "${_TARGET}-${_MODULE_TARGET}-Resources")
+
+            if(NOT TARGET "${_MODULE_RESOURCE_COPY_TARGET}")
+                set(_MODULE_RESOURCE_COPY_COMMANDS)
+                get_target_property(_MODULE_RESOURCE_SOURCE_DIRECTORY ${_MODULE_TARGET} SOURCE_DIR)
+                foreach(_RESOURCE ${${_MODULE}_MODULE_RESOURCES})
+                    set(_MODULE_RESOURCE_SOURCE "${_MODULE_RESOURCE_SOURCE_DIRECTORY}/${_RESOURCE}")
+                    set(_MODULE_RESOURCE_DESTINATION "${android-assets-dir}/Resources/Modules/${_MODULE}/${_RESOURCE}")
+                    get_filename_component(_MODULE_RESOURCE_DESTINATION_DIRECTORY "${_MODULE_RESOURCE_DESTINATION}" DIRECTORY)
+
+                    list(APPEND _MODULE_RESOURCE_COPY_COMMANDS
+                        COMMAND ${CMAKE_COMMAND} -E make_directory "${_MODULE_RESOURCE_DESTINATION_DIRECTORY}")
+
+                    if(IS_DIRECTORY "${_MODULE_RESOURCE_SOURCE}")
+                        list(APPEND _MODULE_RESOURCE_COPY_COMMANDS
+                            COMMAND ${CMAKE_COMMAND} -E rm -rf "${_MODULE_RESOURCE_DESTINATION}"
+                            COMMAND ${CMAKE_COMMAND} -E copy_directory "${_MODULE_RESOURCE_SOURCE}" "${_MODULE_RESOURCE_DESTINATION}")
+                    elseif(EXISTS "${_MODULE_RESOURCE_SOURCE}")
+                        list(APPEND _MODULE_RESOURCE_COPY_COMMANDS
+                            COMMAND ${CMAKE_COMMAND} -E copy_if_different "${_MODULE_RESOURCE_SOURCE}" "${_MODULE_RESOURCE_DESTINATION}")
+                    else()
+                        message(FATAL_ERROR "Android asset source does not exist: ${_MODULE_RESOURCE_SOURCE}")
+                    endif()
+                endforeach()
+
+                if(_MODULE_RESOURCE_COPY_COMMANDS)
+                    add_custom_target(${_MODULE_RESOURCE_COPY_TARGET}
+                        ${_MODULE_RESOURCE_COPY_COMMANDS}
+                        COMMENT "Copying Android resources for ${_MODULE_TARGET}"
+                        VERBATIM)
+                endif()
+            endif()
+
+            if(TARGET "${_MODULE_RESOURCE_COPY_TARGET}")
+                add_dependencies(${_TARGET} ${_MODULE_RESOURCE_COPY_TARGET})
+            endif()
         endif()
     endforeach()
 endmacro()
 
 macro(rayne_copy_resources _TARGET _RESOURCES _ADDITIONAL_PACK_PARAMS)
+    set(_RESOURCE_COPY_TARGET "${_TARGET}-Resources")
+    if(TARGET "${_RESOURCE_COPY_TARGET}")
+        message(FATAL_ERROR "rayne_copy_resources was called more than once for target ${_TARGET}")
+    endif()
+
+    set(_RESOURCE_COPY_COMMANDS)
+
     if(ANDROID)
         rayne_get_android_assets_directory(android-assets-dir)
         file(MAKE_DIRECTORY "${android-assets-dir}")
@@ -184,57 +207,70 @@ macro(rayne_copy_resources _TARGET _RESOURCES _ADDITIONAL_PACK_PARAMS)
     endif()
 
     foreach(_RESOURCE ${_RESOURCES})
+        set(_RESOURCE_SOURCE "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}")
+
         if(ANDROID)
-            set(_RESOURCE_SOURCE "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}")
             set(_RESOURCE_DESTINATION "${android-assets-dir}/${_RESOURCE}")
-
-            if(IS_DIRECTORY "${_RESOURCE_SOURCE}")
-                file(GLOB_RECURSE _RAYNE_ANDROID_RESOURCE_INPUTS LIST_DIRECTORIES false CONFIGURE_DEPENDS "${_RESOURCE_SOURCE}/*")
-                file(REMOVE_RECURSE "${_RESOURCE_DESTINATION}")
-                execute_process(COMMAND ${Python_EXECUTABLE} "${DIR_OF_RAYNE_CMAKE}/../Tools/ResourcePacker/pack.py" "${_RESOURCE_SOURCE}" "${_RESOURCE_DESTINATION}" android ${_ADDITIONAL_PACK_PARAMS} COMMAND_ERROR_IS_FATAL ANY)
-            else()
-                INTERNAL_rayne_stage_android_asset("${_RESOURCE_SOURCE}" "${_RESOURCE_DESTINATION}")
-            endif()
+            set(_RESOURCE_PLATFORM android)
+            set(_REMOVE_RESOURCE_DESTINATION TRUE)
         elseif(APPLE)
-            if(IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}")
-                if(IOS OR VISIONOS)
-                    string(FIND ${CMAKE_OSX_SYSROOT} "XROS" IS_VISIONOS)
-                    string(FIND ${CMAKE_OSX_SYSROOT} "iPhoneSimulator" IS_IOS_SIMULATOR)
-                    string(FIND ${CMAKE_OSX_SYSROOT} "XRSimulator" IS_VISIONOS_SIMULATOR)
+            if(IOS OR VISIONOS)
+                string(FIND ${CMAKE_OSX_SYSROOT} "XROS" IS_VISIONOS)
+                string(FIND ${CMAKE_OSX_SYSROOT} "iPhoneSimulator" IS_IOS_SIMULATOR)
+                string(FIND ${CMAKE_OSX_SYSROOT} "XRSimulator" IS_VISIONOS_SIMULATOR)
 
-                    #Different apple platforms require different shader binaries...
-                    set(RN_IOS_SHADER_TYPE ios)
-                    if(IS_IOS_SIMULATOR GREATER -1)
-                        set(RN_IOS_SHADER_TYPE ios_sim)
-                    elseif(IS_VISIONOS GREATER -1)
-                        set(RN_IOS_SHADER_TYPE visionos)
-                    elseif(IS_VISIONOS_SIMULATOR GREATER -1)
-                        set(RN_IOS_SHADER_TYPE visionos_sim)
-                    endif()
-
-                    add_custom_command(TARGET ${_TARGET} PRE_BUILD COMMAND ${Python_EXECUTABLE} "${DIR_OF_RAYNE_CMAKE}/../Tools/ResourcePacker/pack.py" "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}" "$<TARGET_BUNDLE_CONTENT_DIR:${_TARGET}>/ResourceFiles/${_RESOURCE}" ${RN_IOS_SHADER_TYPE} ${_ADDITIONAL_PACK_PARAMS})
-                else()
-                    add_custom_command(TARGET ${_TARGET} PRE_BUILD COMMAND ${Python_EXECUTABLE} "${DIR_OF_RAYNE_CMAKE}/../Tools/ResourcePacker/pack.py" "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}" "$<TARGET_BUNDLE_CONTENT_DIR:${_TARGET}>/Resources/${_RESOURCE}" macos ${_ADDITIONAL_PACK_PARAMS})
+                #Different apple platforms require different shader binaries...
+                set(_RESOURCE_PLATFORM ios)
+                if(IS_IOS_SIMULATOR GREATER -1)
+                    set(_RESOURCE_PLATFORM ios_sim)
+                elseif(IS_VISIONOS GREATER -1)
+                    set(_RESOURCE_PLATFORM visionos)
+                elseif(IS_VISIONOS_SIMULATOR GREATER -1)
+                    set(_RESOURCE_PLATFORM visionos_sim)
                 endif()
+
+                set(_RESOURCE_DESTINATION "$<TARGET_BUNDLE_CONTENT_DIR:${_TARGET}>/ResourceFiles/${_RESOURCE}")
             else()
-                if(IOS OR VISIONOS)
-                    add_custom_command(TARGET ${_TARGET} PRE_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}" "$<TARGET_BUNDLE_CONTENT_DIR:${_TARGET}>/ResourceFiles/${_RESOURCE}")
-                else()
-                    add_custom_command(TARGET ${_TARGET} PRE_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}" "$<TARGET_BUNDLE_CONTENT_DIR:${_TARGET}>/Resources/${_RESOURCE}")
-                endif()
+                set(_RESOURCE_DESTINATION "$<TARGET_BUNDLE_CONTENT_DIR:${_TARGET}>/Resources/${_RESOURCE}")
+                set(_RESOURCE_PLATFORM macos)
             endif()
+
+            set(_REMOVE_RESOURCE_DESTINATION FALSE)
         elseif(WIN32)
-            if(IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}")
-                add_custom_command(TARGET ${_TARGET} PRE_BUILD COMMAND ${Python_EXECUTABLE} "${DIR_OF_RAYNE_CMAKE}/../Tools/ResourcePacker/pack.py" "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}" "$<TARGET_FILE_DIR:${_TARGET}>/${_RESOURCE}" windows ${_ADDITIONAL_PACK_PARAMS})
-            else()
-                add_custom_command(TARGET ${_TARGET} PRE_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}" "$<TARGET_FILE_DIR:${_TARGET}>/${_RESOURCE}")
-            endif()
+            set(_RESOURCE_DESTINATION "$<TARGET_FILE_DIR:${_TARGET}>/${_RESOURCE}")
+            set(_RESOURCE_PLATFORM windows)
+            set(_REMOVE_RESOURCE_DESTINATION FALSE)
         else()
-            if(IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}")
-                add_custom_command(TARGET ${_TARGET} PRE_BUILD COMMAND ${Python_EXECUTABLE} "${DIR_OF_RAYNE_CMAKE}/../Tools/ResourcePacker/pack.py" "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}" "$<TARGET_FILE_DIR:${_TARGET}>/${_RESOURCE}" linux ${_ADDITIONAL_PACK_PARAMS})
-            else()
-                add_custom_command(TARGET ${_TARGET} PRE_BUILD COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_SOURCE_DIR}/${_RESOURCE}" "$<TARGET_FILE_DIR:${_TARGET}>/${_RESOURCE}")
+            set(_RESOURCE_DESTINATION "$<TARGET_FILE_DIR:${_TARGET}>/${_RESOURCE}")
+            set(_RESOURCE_PLATFORM linux)
+            set(_REMOVE_RESOURCE_DESTINATION FALSE)
+        endif()
+
+        get_filename_component(_RESOURCE_DESTINATION_DIRECTORY "${_RESOURCE_DESTINATION}" DIRECTORY)
+        list(APPEND _RESOURCE_COPY_COMMANDS
+            COMMAND ${CMAKE_COMMAND} -E make_directory "${_RESOURCE_DESTINATION_DIRECTORY}")
+
+        if(IS_DIRECTORY "${_RESOURCE_SOURCE}")
+            if(_REMOVE_RESOURCE_DESTINATION)
+                list(APPEND _RESOURCE_COPY_COMMANDS
+                    COMMAND ${CMAKE_COMMAND} -E rm -rf "${_RESOURCE_DESTINATION}")
             endif()
+
+            list(APPEND _RESOURCE_COPY_COMMANDS
+                COMMAND ${Python_EXECUTABLE} "${DIR_OF_RAYNE_CMAKE}/../Tools/ResourcePacker/pack.py" "${_RESOURCE_SOURCE}" "${_RESOURCE_DESTINATION}" "${_RESOURCE_PLATFORM}" ${_ADDITIONAL_PACK_PARAMS})
+        elseif(EXISTS "${_RESOURCE_SOURCE}")
+            list(APPEND _RESOURCE_COPY_COMMANDS
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different "${_RESOURCE_SOURCE}" "${_RESOURCE_DESTINATION}")
+        else()
+            message(FATAL_ERROR "Resource source does not exist: ${_RESOURCE_SOURCE}")
         endif()
     endforeach()
+
+    if(_RESOURCE_COPY_COMMANDS)
+        add_custom_target(${_RESOURCE_COPY_TARGET}
+            ${_RESOURCE_COPY_COMMANDS}
+            COMMENT "Copying resources for ${_TARGET}"
+            VERBATIM)
+        add_dependencies(${_TARGET} ${_RESOURCE_COPY_TARGET})
+    endif()
 endmacro()
