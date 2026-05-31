7e898ff8dfd351d2e6a59cd9bbe020a03d7f4c58
Nils Daumann <nils_daumann@slindev.com>
2026-05-31 11:55:16 +0200

    Metal: Made post processing override depth testing.

--- Modules/Metal/Sources/RNMetalRenderer.cpp
+++ Modules/Metal/Sources/RNMetalRenderer.cpp
@@ -781,6 +781,9 @@ namespace RN
 				{
 					Mesh *planeMesh = Mesh::WithTexturedPlane(Quaternion::WithEulerAngle(Vector3(0.0f, 90.0f, 0.0f)), Vector3(0.0f), Vector2(1.0f, 1.0f));
 					Material *planeMaterial = Material::WithShaders(GetDefaultShader(Shader::Type::Vertex, nullptr), GetDefaultShader(Shader::Type::Fragment, nullptr));
+					planeMaterial->SetDepthWriteEnabled(false);
+					planeMaterial->SetDepthMode(DepthMode::Always);
+					planeMaterial->SetOverride(Material::Override::DepthWrite | Material::Override::GroupDepth);
 
 					_lock.Lock();
 					_defaultPostProcessingDrawable = static_cast<MetalDrawable*>(CreateDrawable());
