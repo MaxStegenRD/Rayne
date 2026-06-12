cb727b0756fc34e2cca44834f5b44cae4b180dc0
Nils Daumann <nils_daumann@slindev.com>
2026-06-12 12:53:14 +0200

    Added debug asserts for null scenenode access from scenenodeattachments.

--- Source/Scene/RNSceneNodeAttachment.cpp
+++ Source/Scene/RNSceneNodeAttachment.cpp
@@ -58,18 +58,21 @@ namespace RN
 
 	void SceneNodeAttachment::SetWorldPosition(const Vector3 &position)
 	{
+		RN_DEBUG_ASSERT(_node, "SceneNodeAttachment must be attached to a SceneNode before setting world position.");
 		_consumeChangeSets |= SceneNode::ChangeSet::Position;
 		_node->SetWorldPosition(position);
 		_consumeChangeSets &= ~SceneNode::ChangeSet::Position;
 	}
 	void SceneNodeAttachment::SetWorldScale(const Vector3 &scale)
 	{
+		RN_DEBUG_ASSERT(_node, "SceneNodeAttachment must be attached to a SceneNode before setting world scale.");
 		_consumeChangeSets |= SceneNode::ChangeSet::Position;
 		_node->SetWorldScale(scale);
 		_consumeChangeSets &= ~SceneNode::ChangeSet::Position;
 	}
 	void SceneNodeAttachment::SetWorldRotation(const Quaternion &rotation)
 	{
+		RN_DEBUG_ASSERT(_node, "SceneNodeAttachment must be attached to a SceneNode before setting world rotation.");
 		_consumeChangeSets |= SceneNode::ChangeSet::Position;
 		_node->SetWorldRotation(rotation);
 		_consumeChangeSets &= ~SceneNode::ChangeSet::Position;
@@ -77,27 +80,33 @@ namespace RN
 
 	Vector3 SceneNodeAttachment::GetWorldPosition() const
 	{
+		RN_DEBUG_ASSERT(_node, "SceneNodeAttachment must be attached to a SceneNode before getting world position.");
 		return _node->GetWorldPosition();
 	}
 	Vector3 SceneNodeAttachment::GetWorldScale() const
 	{
+		RN_DEBUG_ASSERT(_node, "SceneNodeAttachment must be attached to a SceneNode before getting world scale.");
 		return _node->GetWorldScale();
 	}
 	Quaternion SceneNodeAttachment::GetWorldRotation() const
 	{
+		RN_DEBUG_ASSERT(_node, "SceneNodeAttachment must be attached to a SceneNode before getting world rotation.");
 		return _node->GetWorldRotation();
 	}
 
 	Vector3 SceneNodeAttachment::GetForward() const
 	{
+		RN_DEBUG_ASSERT(_node, "SceneNodeAttachment must be attached to a SceneNode before getting forward vector.");
 		return _node->GetForward();
 	}
 	Vector3 SceneNodeAttachment::GetUp() const
 	{
+		RN_DEBUG_ASSERT(_node, "SceneNodeAttachment must be attached to a SceneNode before getting up vector.");
 		return _node->GetUp();
 	}
 	Vector3 SceneNodeAttachment::GetRight() const
 	{
+		RN_DEBUG_ASSERT(_node, "SceneNodeAttachment must be attached to a SceneNode before getting right vector.");
 		return _node->GetRight();
 	}
 } // namespace RN
